328d7f8e9a102ef4ce8e434428fa87dd2d7e38d4
Tae <cjsxorud111@gmail.com>
2023-09-01 23:36:58 +0900

    주석제거

--- application/controllers/trip_plan_controller/Plan.php
+++ application/controllers/trip_plan_controller/Plan.php
@@ -16,15 +16,9 @@ class Plan extends CI_Controller
 
 	public function store() {
 
-		// $datas 배열을 활용하여 원하는 처리 수행
+		$result = $this->Plan_model->store();
 
-		// 예를 들어, 데이터를 모델을 통해 데이터베이스에 저장하려면:
-		$this->Plan_model->store();
-
-		// 다음으로 이동하거나 결과를 표시하는 등의 작업 수행
-
-		// 예산계산 페이지로 리디렉션하거나 다른 작업 수행
-		// redirect('다음_페이지_주소');
+		$this->load->view('trip_plan/schedule', $result);
 	}
 
 }
